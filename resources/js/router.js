a6cd8f702157c9b6b175864f87fc9fc387bebdbe
mjj0324 <mjj0324@gmail.com>
2019-06-11 06:24:58 +0900

    router 수정

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,76 +1,30 @@
 var ROUTER;
 (function(){
-    var hostname = '';
-    var status = {};
-    var getStatus;
+    var router;
 
-    getStatus = function(ag){
-        var page, idx;
-
-        page = ag[0];
-        idx = ag.length > 1 ? ag[1] : 0;            // view처럼 idx 필요할 때 파라미터로 넘길 수 있도록
-console.log(page);        
-        switch(page){
-            case ('MAIN' || '' || undefined):
-                status = {
-                    name: 'MAIN',
-                    url: '/index.html',
-                    tit: '메인',
-                    cb: function(){
-                        if(arguments[0] === 'destroy') MAIN.destroy();
-                        else MAIN.init();
-                    }
-                };
-                break;
-
-            case 'LIST':
-                status = {
-                    name: 'LIST',
-                    url: '/list',
-                    tit: '리스트',
-                    cb: function(){
-                        if(arguments[0] === 'destroy') LIST.destroy();
-                        else LIST.init();
-                    }
-                };
-                break;
-
-            case 'VIEW':
-                status = {
-                    name: 'VIEW',
-                    url: '/view',
-                    tit: '콘텐츠',
-                    cb: function(){
-                        if(arguments[0] === 'destroy') VIEW.destroy();
-                        else VIEW.init(idx);
-                    }
-                };
-                break;
-
-            default:
-                break;
+    router = {
+        '/': function(){
+            MAIN.init();
+        },
+        '/list': function(){
+            LIST.init();
+        },
+        '/view': function(idx){
+            console.log(idx);
+            VIEW.init(idx);
         }
     };
 
     ROUTER = {
         init: function(){
-            var self = this;
-            self.go('MAIN');
+            var url = arguments[0] !== undefined ? arguments[0] : '/';      // arguments[0] 은 url
 
-            if(status.url !== undefined){
-                window.onpopstate = function(e){
-                    self.go(e.state);
-                }
+            window.onpopstate = function(e){
+                router[e.state](arguments[1]);
             }
-        },
-        go: function(){
-            if(status.url !== undefined) {
-                status.cb('destroy');
-            }
-            
-            getStatus(arguments);
-            history.pushState(status.name, status.tit, hostname + status.url);
-            status.cb();
+
+            history.pushState(url, null, url);
+            router[url](arguments[1]);              
         }
     };
 })();
